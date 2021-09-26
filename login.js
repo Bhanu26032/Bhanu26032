beaaebba2108782eca6f67fa8c583fd53daf0f97
Bhanu26032 <87572638+Bhanu26032@users.noreply.github.com>
2021-09-26 17:01:24 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate()
 {
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
-if(username=="SAITEJA"&& password=="09-27-2001")
+if(username=="SAITEJA"&& password=="2001-09-27")
 {
  window.location.href="bdayanim.html";
 return false;
